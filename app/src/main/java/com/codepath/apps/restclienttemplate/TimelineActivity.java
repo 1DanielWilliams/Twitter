300b6395c89aead95a989c4a0449e0328b2492b7
1DanielWilliams <84046935+1DanielWilliams@users.noreply.github.com>
2022-06-07 14:29:09 -0700

    step 8

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -1,6 +1,7 @@
  package com.codepath.apps.restclienttemplate;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -18,6 +19,7 @@ import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.parceler.Parcels;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,6 +29,7 @@ import okhttp3.Headers;
  public class TimelineActivity extends AppCompatActivity {
 
      public static final String TAG = "TimelineActivity";
+     private final int REQUEST_CODE = 20;
 
     TwitterClient client;
     RecyclerView rvTweets;
@@ -64,11 +67,28 @@ import okhttp3.Headers;
         switch (item.getItemId()) {
             case R.id.compose:
                 //compose icon has be selected
-                Toast.makeText(this, "Compose!", Toast.LENGTH_SHORT).show();
                 //navigate to the compose activity
+                Intent i = new Intent(this, ComposeActivity.class);
+                startActivityForResult(i, REQUEST_CODE);
                 return true;
         }
          return super.onOptionsItemSelected(item);
+     }
+
+     @Override
+     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+         if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) { //checks if the result succeeded with the correct code
+             // Get data from the intent (tweet)
+             Tweet tweet = Parcels.unwrap(data.getParcelableExtra("tweet"));
+             // Update the RV with the tweet
+             // Modify data source of the tweets
+             tweets.add(0, tweet);
+             // Update the adapter
+             adapter.notifyItemInserted(0);
+             // Smooth scroll so that a user does not have to scroll up to see tweet
+             rvTweets.smoothScrollToPosition(0);
+         }
+        super.onActivityResult(requestCode, resultCode, data);
      }
 
      private void populateHomeTimeLine() {
